2453f8f2997aa017a7f61fd807e0182d825ea549
sujal_0510 <sujalpatel0510@gmail.com>
2025-08-02 09:01:06 +0530

    user model created

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://quickuser:sujal@localhost/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+#user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(10), default='user')  # user, agent, admin
+    tickets = db.relationship('Ticket', backref='user', lazy=True)
